01137e1e8ff319d686494a6cb38355e69e1d61e5
ZeroBytes <89136241+RenanRezende3301@users.noreply.github.com>
2025-03-06 11:58:28 -0300

    1.0

---
+++ studies004/ex001/mq001/scripts/music.js
@@ -0,0 +1,20 @@
+document.getElementById('pauseButton').addEventListener('click', function() {
+    var audio = document.getElementById('music');
+    if (audio.paused) {
+        audio.play();  
+        this.textContent = 'stop';  
+    } else {
+        audio.pause();  
+        this.textContent = 'play';
+    }
+});
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    var audio = document.getElementById('music');
+    audio.volume = 0.1; 
+});
+
+function clickbit() {
+    var sound = document.getElementById('audio');
+    sound.play();
+   }
