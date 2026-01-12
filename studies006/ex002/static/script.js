514392088c09ccdc04f4bc03ea21fb3ee967af54
RenanRezende3301 <renanrezende0133@hotmail.com>
2026-01-12 15:46:56 -0300

    add folder studies006 and some exercises

---
+++ studies006/ex002/static/script.js
@@ -0,0 +1,40 @@
+
+/* Embedding */
+const name = "Renan";
+const welcome = `Hello, ${name}`;
+console.log(welcome);
+
+/* space */
+console.log(".");
+console.log("next test below");
+console.log(".");
+
+/* Embedding two variables */
+const one = "Welcome";
+const two = "Renan";
+const joined = `${one} ${two}`
+console.log(joined)
+
+
+/* space */
+console.log(".");
+console.log("next test below");
+console.log(".");
+
+
+/* number vs string */
+const test = 123;
+console.log(typeof test);
+
+/* space */
+console.log(".");
+console.log("next test below");
+console.log(".");
+
+/* convert number/string */
+const mynumber = "two";
+const convertnumber = Number(mynumber);
+console.log(typeof convertnumber);
+
+
+
---
+++ studies006/ex003/static/script.js
@@ -0,0 +1,28 @@
+
+/* Length */
+let text = "1234567";
+let characters = text.length;
+
+console.log(characters);
+
+/* space */
+console.log(".");
+console.log("Extracting String Characters -- charAt *");
+console.log(".");
+
+/* Extracting String Characters -- at or charAt*/
+let tex2 = "abcdefgh";
+let extracting = tex2.at(0);
+
+console.log(extracting);
+
+/* space */
+console.log(".");
+console.log("Extracting String Characters -- charCodeAt");
+console.log(".");
+
+/* Extracting String Characters -- charCodeAt */
+let text3 = "HELLO WORLD";
+let code = text3.codePointAt(1);
+
+console.log(code)
